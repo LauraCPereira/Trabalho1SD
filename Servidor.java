489205b22a97960d62b3f34ebc94c19574b7b15c
Robson Ronzani <ronzani.robson@gmail.com>
2019-02-02 11:51:36 -0300

    erro

--- Servidor.java
+++ Servidor.java
@@ -2,21 +2,34 @@ import java.net.ServerSocket;
 import java.io.IOException;
 import java.util.Scanner;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
   public static void main(String[] args) throws IOException {
-    ServerSocket servidor = new ServerSocket(12345);
-    System.out.println("Porta 12345 aberta!");
-
-    Socket cliente = servidor.accept();
-    System.out.println("Nova conexao com o cliente " + cliente.getInetAddress().getHostAddress());
-
-    Scanner entrada = new Scanner(cliente.getInputStream());
-    while(entrada.hasNextLine()) {
-      System.out.println("Mensagem recebida: " + entrada.nextLine());
+   try {
+      ServerSocket server = new ServerSocket(1234);
+      Socket clSocket = server.accept();
+      InputStream in = clSocket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(in);
+      BufferedReader reader = new BufferedReader(isr);
+      File f1 = new File("/home/robson/pos/laura/Trabalho1SD/laura_recebido.txt");
+      FileOutputStream out = new FileOutputStream(f1);
+          int tamanho = 4096; // buffer de 4KB  
+          byte[] buffer = new byte[tamanho];  
+          int lidos = -1;  
+          while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+            System.out.println(lidos);
+              out.write(buffer, 0, lidos);  
+          }  
+          out.flush();  
+    } catch (IOException e) {
     }
-
-    entrada.close();
-    servidor.close();
   }
 }
